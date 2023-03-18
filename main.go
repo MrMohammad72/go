6597f0c806ec0acc120681e3363b2277159bc19b
MrMohammad72 <m.datkhah@yahoo.com>
2023-03-18 15:16:27 +0330

    Using switch instead of if

--- main.go
+++ main.go
@@ -3,16 +3,15 @@ package main
 import "fmt"
 
 func comp(num1 int, num2 int) (isEquall bool, diff int) {
-	if num1 > num2 {
-		//return false, num1 - num2
+
+	switch {
+	case num1 > num2:
 		isEquall = false
 		diff = num1 - num2
-	} else if num2 > num1 {
-		//return false, num2 - num1
+	case num2 > num1:
 		isEquall = false
 		diff = num2 - num1
-	} else {
-		//return true, 0
+	default:
 		isEquall = true
 		diff = 0
 	}
