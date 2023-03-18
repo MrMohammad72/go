f05f4ad36af4fc882c77287db1b93a576904eee3
MrMohammad72 <m.datkhah@yahoo.com>
2023-03-18 10:04:43 +0330

    The second mode of defining the function is to use it name type at the exit

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import "fmt"
 
-func comp(num1 int, num2 int) (bool, int) {
+func comp(num1 int, num2 int) (isEquall bool, diff int) {
 	if num1 > num2 {
-		return false, num1 - num2
+		//return false, num1 - num2
+		isEquall = false
+		diff = num1 - num2
 	} else if num2 > num1 {
-		return false, num2 - num1
+		//return false, num2 - num1
+		isEquall = false
+		diff = num2 - num1
 	} else {
-		return true, 0
+		//return true, 0
+		isEquall = true
+		diff = 0
 	}
+	return
 
 }
 
 func main() {
 
-	fmt.Println(comp(1, 2))
+	fmt.Println(comp(1, 1))
 }
