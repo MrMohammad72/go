8cd75e99e1efa5947d5cd99863a769a4121b4111
MrMohammad72 <m.datkhah@yahoo.com>
2023-03-18 13:02:02 +0330

    The first mode of function definition

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func comp(num1 int, num2 int) (bool, int) {
+	if num1 > num2 {
+		return false, num1 - num2
+	} else if num2 > num1 {
+		return false, num2 - num1
+	} else {
+		return true, 0
+	}
+
+}
+
+func main() {
+
+	fmt.Println(comp(1, 2))
+}
